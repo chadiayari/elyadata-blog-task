7dc7338ab069f6d720f4da81ce4107d778c14446
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2023-02-12 21:46:58 +0100

    created blog page

--- frontend/src/services/blog-service.js
+++ frontend/src/services/blog-service.js
@@ -9,6 +9,12 @@ class BlogService {
     return response.data;
   }
 
+  async getBlogById(id) {
+    const response = await axios.get(API_URL + "get_blog_by_id/" + id);
+    if (response.data == null) return;
+    return response.data;
+  }
+
   async createBlog(data) {
     const response = await axios.post("create_blog", data);
     return response;
--- frontend/src/views/BlogPage/index.jsx
+++ frontend/src/views/BlogPage/index.jsx
@@ -1,3 +1,38 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import blogService from "../../services/blog-service";
+import Chip from "../../components/Chip";
+
 export const BlogPage = () => {
-  return <>blog content</>;
+  const [blogData, setBlogData] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    blogService.getBlogById(id).then((data) => setBlogData(data[0]));
+  }, []);
+
+  return (
+    <>
+      {blogData ? (
+        <div className="blog-wrap">
+          <center>
+            <p className="blog-date">Published by {blogData.author}</p>
+            <h1>{blogData.blog_name}</h1>
+            <div className="blog-subCategory">
+              <div>
+                <Chip label={blogData.blog_name} />
+              </div>
+            </div>
+          </center>
+          <img src={blogData.blog_image} alt="cover" />
+          <div
+            className="blog-content"
+            dangerouslySetInnerHTML={{ __html: blogData.blog_content }}
+          ></div>
+        </div>
+      ) : (
+        <p>Content not found</p>
+      )}{" "}
+    </>
+  );
 };
